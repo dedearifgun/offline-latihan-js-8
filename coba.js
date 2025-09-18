58415f7696e9ed761dd840c8b596a8c8a84054ab
dedearifgun <dedearifgunawan@gmail.com>
2025-09-18 09:25:51 +0700

    module moment

--- coba.js
+++ coba.js
@@ -1,4 +1,16 @@
+//function cetakNama(nama) {
+//    return `Halo, nama saya ${nama}`;
+//}
+//const PI =3.14;
+//module.exports = cetakNama;
+//module.exports.PI = PI;
+// module.exports = {cetakNama, PI}; // cara lain export banyak
+
 function cetakNama(nama) {
     return `Halo, nama saya ${nama}`;
 }
-module.exports = cetakNama;
+
+const PI = 3.14;
+
+// Export banyak sekaligus
+module.exports = { cetakNama, PI };
--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-const cetakNama = require('./coba.js')
-console.log(cetakNama('Tika'))
+//const cetakNama = require('./coba.js')
+//console.log(cetakNama('Tika'))
+
+//const fs = require('fs');
+//const coba =require('coba.js')
+//const moment = require('moment');
+
+//console.log (coba.cetakNama('Tika'),coba.PI);
+
+
+// import modul
+const coba = require('./coba.js');
+
+// bisa panggil function
+console.log(coba.cetakNama('Tika'));
+
+// bisa akses variabel
+console.log(coba.PI);
